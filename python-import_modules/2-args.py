a0442b5d6618913474652270cac997c3fc1a0d36
Your Name <you@example.com>
2026-01-15 10:18:24 +0000

    Fixed code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,14 +3,16 @@ from sys import argv
 
 if __name__ == "__main__":
 
+    argc = len(argv) - 1
+    
     if len(argv) - 1 == 1:
-        print("{} argument:".format(len(argv) - 1))
+        print("{} argument:".format(argc))
 
     elif len(argv) - 1 == 0:
-        print("{} argument.".format(len(argv) - 1))
+        print("{} argument.".format(argc))
 
     else:
-        print("{} arguments:".format(len(argv) - 1))
+        print("{} arguments:".format(argc))
 
-    for i, args in enumerate(argv[1:], start=1):
+    for i, args in enumerate(argv[1:], 1):
         print("{}: {}".format(i, args))
