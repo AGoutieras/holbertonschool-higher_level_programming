64eb0d257bd6e64d5c00040e2d62715e424cbda5
Your Name <you@example.com>
2026-01-15 12:36:57 +0000

    Fixed code

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    from sys import argv
+    import sys
 
-    operator = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
 
-    if len(argv) - 1 != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     if operator not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
     if operator == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
 
     elif operator == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
 
     elif operator == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
 
     elif operator == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
