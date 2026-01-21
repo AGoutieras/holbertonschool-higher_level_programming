c8227fa57765cf0f9f35f57850e68e5a77613d5f
Your Name <you@example.com>
2026-01-21 08:15:36 +0000

    Fixed code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+    for row in matrix:
+        for elem in row:
+            print("{}".format(elem), end="")
         print()
