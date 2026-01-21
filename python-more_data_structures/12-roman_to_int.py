f4f0e820c1b31a53796b8e5096af5dfa157ced0a
Your Name <you@example.com>
2026-01-21 11:39:28 +0000

    Added 12-roman_to_int.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string == "":
+        return 0
+
+    total = 0
+    i = 0
+
+    rmn_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+
+    while i < len(roman_string):
+        value = rmn_dict[roman_string[i]]
+
+        if i + 1 < len(roman_string) and rmn_dict[roman_string[i + 1]] > value:
+            total += rmn_dict[roman_string[i + 1]] - value
+            i += 2
+
+        else:
+            total += value
+            i += 1
+
+    return total
