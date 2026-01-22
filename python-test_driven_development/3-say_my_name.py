3a15575223042ad5942ad3e5e6f304dd68ab850e
Your Name <you@example.com>
2026-01-22 13:54:28 +0000

    fixed docstyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Prints the full name in the format 'My name is <first_name> <last_name>'."""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints the full name in the format 'My name is <first_name> <last_name>'.
