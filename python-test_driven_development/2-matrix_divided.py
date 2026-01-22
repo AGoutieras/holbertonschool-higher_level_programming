ca438cc21fb4ed7945ba51faa6965c802ed5debd
Your Name <you@example.com>
2026-01-22 16:28:28 +0000

    fixed pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,12 +23,12 @@ def matrix_divided(matrix, div):
     if (not isinstance(matrix, list) or
             not all(isinstance(row, list) for row in matrix)):
         raise TypeError(
-        "matrix must be a matrix (list of lists) of integers/floats")
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     if (not all(all(isinstance(elem, (int, float)) for elem in row)
                 for row in matrix)):
         raise TypeError(
-        "matrix must be a matrix (list of lists) of integers/floats")
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
