0c9419bb4f3be752600a3d80b05eb4e92c6c21fc
Your Name <you@example.com>
2026-01-22 11:36:48 +0000

    Fixed doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""
-Computes the sum of two numbers as integers.
-
-Prototype: def add_integer(a, b=98):
-- Both a and b must be int or float; if not, raises a TypeError
-  with the message 'a must be an integer' or 'b must be an integer'.
-- If a or b are floats, they are first converted to integers.
-- Returns an integer representing the sum of a and b.
-- No external modules are required or allowed.
-"""
+"""Defines an integer addition function."""
 
 
 def add_integer(a, b=98):
-    """
-    Calculates the sum of two numbers as integers.
+    """Return the integer addition of a and b.
 
-    Parameters:
-        a (int or float): The first value to add.
-        b (int or float, optional): The second value to add (default is 98).
+    Float arguments are typecasted to ints before addition is performed.
 
-    Returns:
-        int: The result of adding a and b as an integer.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
