81c1eb2ec5f83703ca0e6d2c273e86706a89cdfd
Your Name <you@example.com>
2026-01-22 10:03:01 +0000

    Added documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+adds two numbers as integers
+"""
+
+
 def add_integer(a, b=98):
+    """
+    Adds two integers (or floats casted to int).
+
+    a and b must be integers or floats; floats are casted to integers.
+    Returns the sum as an integer.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
