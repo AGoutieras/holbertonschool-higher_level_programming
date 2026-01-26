991912565d04fe14e1e5d497834000a68d22d3ed
Your Name <you@example.com>
2026-01-26 08:50:12 +0000

    Added 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,11 +4,4 @@
 
 class Square:
     """Represents a square."""
-    def __init__(self, size):
-        """
-        Initializes a new Square instance.
-
-        Args:
-            size: the size of the square
-        """
-        self.__size = size
+    pass
