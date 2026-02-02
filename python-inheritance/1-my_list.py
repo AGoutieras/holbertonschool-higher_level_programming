e13820bfb04dd9aec56c9b32d7f474e0c8e1deb7
AGoutieras <goutieras.anthony@gmail.com>
2026-02-02 09:46:52 +0000

    Added 1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define an inherited list class MyList"""
+
+
+class MyList(list):
+    """Inherit from list and add a print_sorted method"""
+    def print_sorted(self):
+        """Print the list in ascending order"""
+        print(sorted(self))
