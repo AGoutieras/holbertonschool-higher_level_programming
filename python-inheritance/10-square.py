df994129e8d849f2e9e7a955635019c37584fdbf
AGoutieras <goutieras.anthony@gmail.com>
2026-02-04 09:37:05 +0000

    Added import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,33 +2,7 @@
 """Define a base geometry class 'BaseGeometry'"""
 
 
-class BaseGeometry:
-    """Represents BaseGeometry"""
-    def integer_validator(self, name, value):
-        """Checks if value is an int and is greater than 0"""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """Represents a rectangle"""
-    def __init__(self, width, height):
-        """Initialize rectangle with validated width and height"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Return the area of the rectangle."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the string representation of the rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
