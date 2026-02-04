a9511aede5be0fbf119785efa76d5f695ba55f75
AGoutieras <goutieras.anthony@gmail.com>
2026-02-04 09:51:50 +0000

    Fixed code

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -52,5 +52,5 @@ class Rectangle(Shape):
 
 def shape_info(obj):
     """Print the area and perimeter of a shape."""
-    print("Area:", obj.area())
-    print("Perimeter:", obj.perimeter())
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
