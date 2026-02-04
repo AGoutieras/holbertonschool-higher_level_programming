0eab495f623c730b2c7c796984d879aae5575319
AGoutieras <goutieras.anthony@gmail.com>
2026-02-04 09:33:19 +0000

    Refactor code

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Check if an object is an inherited instance of a given class"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    return False if type(obj) is a_class else issubclass(type(obj), a_class)
