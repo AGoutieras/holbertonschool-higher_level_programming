f538e6778ed65098bc15bf0ac4e7fc7130f4c926
AGoutieras <goutieras.anthony@gmail.com>
2026-02-04 09:11:13 +0000

    Added space below class docstring

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,8 @@
 
 class MyList(list):
     """Inherit from list and add a print_sorted method"""
+
+
     def print_sorted(self):
         """Print the list in ascending order"""
         print(sorted(self))
