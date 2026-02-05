d0b937a20ce0c79c36da6ca8ec2bb15bcb6f7d6a
AGoutieras <goutieras.anthony@gmail.com>
2026-02-05 08:41:22 +0000

    Fixed docstring

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""Define an inherited list class MyList"""
+"""
+Define an inherited list class MyList
+"""
 
 
 class MyList(list):
-    """Inherit from list and add a print_sorted method"""
+    """
+    Inherit from list and add a print_sorted method
+    """
     def print_sorted(self):
-        """Print the list in ascending order"""
+        """
+        Print the list in ascending order
+        """
         print(sorted(self))
