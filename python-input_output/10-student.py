03ab1c48d19cda4bfca4c2c12b925b2de79a99b4
AGoutieras <goutieras.anthony@gmail.com>
2026-02-09 01:08:44 +0000

    Added add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list):
+            filtered = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    filtered[key] = self.__dict__[key]
+            return filtered
+        return self.__dict__
+
+    
