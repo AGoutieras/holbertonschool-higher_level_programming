1d44260830406b131348ee6a135b88376ecb03df
AGoutieras <goutieras.anthony@gmail.com>
2026-02-17 12:39:06 +0000

    Added task_02_requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    print(r.status_code)
+
+    if r.status_code == 200:
+        data = r.json()
+
+        for post in data:
+            print(post["title"])
+
+def fetch_and_save_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    if r.status_code == 200:
+        data = r.json()
+
+        filtered_posts = []
+
+        for post in data:
+            post_data = {}
+            post_data["id"] = post["id"]
+            post_data["title"] = post["title"]
+            post_data["body"] = post["body"]
+            filtered_posts.append(post_data)
+
+    with open('posts.csv', 'w', newline='') as f:
+        fieldnames = ['id', 'title', 'body']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(filtered_posts)
