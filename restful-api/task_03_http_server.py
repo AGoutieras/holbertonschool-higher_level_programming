c1bf35291cfee8aa3d2dbff9258d34a2be7f71c5
Anthony Goutieras <goutieras.anthony@gmail.com>
2026-02-18 11:31:17 +0100

    Remove '/info' endpoint and update 404 response
    Removed the '/info' endpoint and simplified the 404 response message.

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -30,18 +30,11 @@ class Server(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
-        elif self.path == "/info":
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            data = {"version": "1.0", "description": "A simple API built with http.server"}
-            self.wfile.write(json.dumps(data).encode())
-
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found: The requested URL was not found on this server.")
+            self.wfile.write(b"404 Not Found.")
 
 
 server = HTTPServer((host, port), Server)
