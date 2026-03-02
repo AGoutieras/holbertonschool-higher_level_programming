7232e4ef8136ccf04462c983b9f280978cc88d3b
AGoutieras <goutieras.anthony@gmail.com>
2026-03-02 09:12:44 +0000

    Added 0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Lists all privileges of the MySQL users 
+-- LIsts all privileges of the user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- LIsts all privileges of the user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
