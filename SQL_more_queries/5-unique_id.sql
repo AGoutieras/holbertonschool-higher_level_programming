9beb6c27d58c4c96c0b66577454270c76ecd3e5d
AGoutieras <goutieras.anthony@gmail.com>
2026-03-02 10:34:12 +0000

    Added 5-unique_id.sql

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Creates the table unique_id with a unique constraint on id
+-- Creates the table unique_id
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
