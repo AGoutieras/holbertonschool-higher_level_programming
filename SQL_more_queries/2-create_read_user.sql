b99dc05f2f15f84b09dce45c6482824e3c0ca5ae
AGoutieras <goutieras.anthony@gmail.com>
2026-03-02 09:32:27 +0000

    Added 2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2 with SELECT privileges
+-- Creates the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Creates the user user_0d_2 with the password user_0d_2_pwd
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grants SELECT privileges to user user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
