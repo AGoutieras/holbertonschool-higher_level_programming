106e168d642eb4eba4b260ead077bf3831c057db
AGoutieras <goutieras.anthony@gmail.com>
2026-03-02 10:51:44 +0000

    Added 6-states.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0d_usa and the table states
+-- Creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Creates the table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL 
+);
