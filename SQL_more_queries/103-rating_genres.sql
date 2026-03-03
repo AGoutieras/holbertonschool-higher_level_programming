decfb3a7f824fc5ba714d5947f1ab1bc42b8c12c
AGoutieras <goutieras.anthony@gmail.com>
2026-03-04 00:55:11 +0100

    Fixed code

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -5,4 +5,4 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
