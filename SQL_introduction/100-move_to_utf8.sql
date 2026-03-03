03999741ee97da6888fa60266343115f0002fb90
AGoutieras <goutieras.anthony@gmail.com>
2026-03-04 00:36:24 +0100

    Added comments

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,9 @@
 -- Converts hbtn_0c_0 database to UTF8
+-- Converts the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Converts the table
 ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Converts the column name
 ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
