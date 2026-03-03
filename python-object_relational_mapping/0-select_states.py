f81f49020e03a357b3e8b7508922a7fe98d210e8
AGoutieras <goutieras.anthony@gmail.com>
2026-03-03 15:56:49 +0100

    Added 0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" lists all State objects from the database hbtn_0e_6_usa """
+
+import sys
+import MySQLdb
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    
+    for state in rows:
+        print(state)
