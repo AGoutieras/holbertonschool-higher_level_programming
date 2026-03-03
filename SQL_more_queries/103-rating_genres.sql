670d8d4ad60e8e16ca9871ae8aca3f4049b9b830
AGoutieras <goutieras.anthony@gmail.com>
2026-03-04 00:54:20 +0100

    Fixed code

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,6 @@
 -- Lists all genres by their rating sum in descending order
 -- Display genres and their rating sum using JOIN
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
