4d8e87c673d37b0b349274d5dff9d0d4c40ea534
AGoutieras <goutieras.anthony@gmail.com>
2026-03-04 11:09:31 +0100

    chmod

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states "
-                   "WHERE BINARY name = '{}'".format(sys.argv[4]))
-
+                   "WHERE name = '{}'"
+                   "ORDER BY id".format(sys.argv[4]))
+    
     [print(state) for state in cursor.fetchall()]
