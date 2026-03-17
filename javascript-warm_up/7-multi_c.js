2920ea671fa2688cba3ccaffe8c5f3936499ecc8
AGoutieras <goutieras.anthony@gmail.com>
2026-03-17 19:01:21 +0100

    feat: print "C is fun" x times or "Missing number of occurrences"

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const x = process.argv[2];
+const x = parseInt(process.argv[2]);
 
-if (isNaN(parseInt(process.argv[2]))) {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
