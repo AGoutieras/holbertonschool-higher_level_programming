36f13e957de909a8bfe9e5aefdbedc89453643eb
AGoutieras <goutieras.anthony@gmail.com>
2026-03-23 14:41:27 +0100

    feat: add SQL data source support in /products route

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request
+import json
+import csv
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+
+    return render_template('items.html', items=data["items"])
+
+
+@app.route('/products')
+def products_display():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    if source == 'json':
+        with open('products.json', 'r') as f:
+            products = json.load(f)
+
+    elif source == 'csv':
+        with open('products.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            products = list(reader)
+
+    elif source == 'sql':
+        db = sqlite3.connect('products.db')
+        db.row_factory = sqlite3.Row
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM Products")
+        products = cursor.fetchall()
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id:
+        found = None
+        for product in products:
+            if product["id"] == int(product_id):
+                found = product
+        if found is None:
+            return render_template('product_display.html', error="Product not found")
+        else:
+            return render_template('product_display.html', products=[found])
+
+    return render_template('product_display.html', products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
