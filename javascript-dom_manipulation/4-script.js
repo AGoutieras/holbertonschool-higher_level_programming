beda1fc653e0a9e5e34297f9b5992fc581da1f01
AGoutieras <goutieras.anthony@gmail.com>
2026-03-23 01:25:59 +0100

    fix: rename my_list to myList to follow camelCase convention

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -3,6 +3,6 @@ const addItem = document.querySelector('#add_item');
 addItem.addEventListener('click', function () {
   const li = document.createElement('li');
   li.textContent = 'Item';
-  const my_list = document.querySelector('.my_list');
-  my_list.appendChild(li);
+  const myList = document.querySelector('.my_list');
+  myList.appendChild(li);
 });
